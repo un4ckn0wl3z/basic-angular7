5917579837222d3e116b21734d3c0100c3572599
un4ckn0wl3z <slickerth@gmail.com>
2019-07-27 19:02:13 +0700

    add binding with attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   header: string = 'Basic Angular 7';
   title: string = 'Basic Angular 7 is ez.';
+  button: string = "เนื้อหาเพิ่มเติม";
 
   constructor() {
     setTimeout(() => this.title = 'คอร์สพื้นฐาน Angular 7 ง่ายมาก', 1000);
