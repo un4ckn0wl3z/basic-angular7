cbb9f478163455412a4cef790e58060e3169221b
un4ckn0wl3z <slickerth@gmail.com>
2019-07-27 18:48:24 +0700

    restore project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,13 @@
-import { MySimpleClass } from './my-simple/my-simple.class';
+
 import { Component } from '@angular/core';
-import { IAppComponent } from './my-simple/app.interface';
-import { MyDecorator } from './my-simple/my.decorator';
-import { MyClass } from './my-simple/my.class';
-
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements IAppComponent {
-  items: string[] = [];
-  title: string = 'basic-angular7';
+export class AppComponent {
 
-  constructor() {
-    // const myClass = new MyClass();
-    // const mySimpleClass = new MySimpleClass();
-
-    // console.log(myClass);
-    // console.log(myClass.squName);
-    // console.log(mySimpleClass.title);
-
-    // console.log(configs);
-    // console.log(configs.target);
-    // console.log(configs.address);
-
-  }
-
-  addItem(item: string) {
-    this.items.push(item);
-  }
-  removeItem(index: number) {
-    this.items.splice(index, 1);
-  }
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { AlertModule } from 'ngx-bootstrap/alert';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,9 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    AlertModule.forRoot()
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
