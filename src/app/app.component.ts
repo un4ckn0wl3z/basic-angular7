d618f08f84f8852279deef128e3c5da54aaddaf1
un4ckn0wl3z <slickerth@gmail.com>
2019-07-27 19:08:36 +0700

    add inner html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   header: string = 'Basic Angular 7';
   title: string = 'Basic Angular 7 is ez.';
   button: string = "เนื้อหาเพิ่มเติม";
+  content: string = `With <a target="_blank" href="https://getbootstrap.com/docs/4.0/components/card/">supporting</a> text below as a natural lead-in to additional content.`;
 
   constructor() {
     setTimeout(() => this.title = 'คอร์สพื้นฐาน Angular 7 ง่ายมาก', 1000);
