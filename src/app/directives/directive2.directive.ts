16ea0368cc48091fd67e747b9d72169537f43ff8
un4ckn0wl3z <slickerth@gmail.com>
2019-07-28 22:51:58 +0700

    add and play with template directive

--- src/app/directives/directive2.directive.ts
+++ src/app/directives/directive2.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, OnChanges, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[directive2]'
 })
-export class Directive2Directive {
+export class Directive2Directive implements OnChanges {
 
-  constructor() { }
+  @Input('directive2') directive2: number;
+
+  constructor(
+    private template: TemplateRef<any>,
+    private container: ViewContainerRef 
+  ) { }
+
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    if((this.directive2 % 2) == 0){
+      this.container.createEmbeddedView(this.template);
+    }else{
+      this.container.remove()
+    }
+  }
 
 }
