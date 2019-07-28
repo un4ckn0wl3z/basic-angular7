8a91c26ebde3c82c264a8be4ff42881b025c9448
un4ckn0wl3z <slickerth@gmail.com>
2019-07-28 19:17:26 +0700

    add decision

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-
-
   message:string = 'Basic input for component';
 
+
+  constructor(){
+
+    const score = 90;
+
+    if(score >= 100){
+      console.log('score >= 100');
+    }else if (score >= 90){
+      console.log('score >= 90');
+    }else if (score >= 80){
+      console.log('score >= 80');
+    }else if (score >= 70){
+      console.log('score >= 70');
+    }else if (score >= 60){
+      console.log('score >= 60');
+    }else{
+      console.log('fuck ');
+    }
+
+
+  }
 
 
   // get output value from com2
