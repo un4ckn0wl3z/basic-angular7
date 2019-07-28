3f5a55560b83e77141a2088bff0c4315c0fc78c3
un4ckn0wl3z <slickerth@gmail.com>
2019-07-28 13:02:01 +0700

    implement onInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,11 @@ export class AppComponent {
 
 
   message:string = 'Basic input for component';
-  @ViewChild('com01') com01: Com01Component;
+
 
 
   // get output value from com2
   onMessage(input: string){
-    //console.log('th is output from com2');
-    this.com01.message = input;
     this.message = input;
   }
 
--- src/app/com01/com01.component.ts
+++ src/app/com01/com01.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-com01',
     templateUrl: './com01.component.html',
     styleUrls: ['./com01.component.css']
   })
-export class Com01Component {
+export class Com01Component implements OnInit {
+
 
   @Input('message') message:string;
 
   constructor(){
     this.message = 'empty';
+    console.log('constructor');
   }
+
+    
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+
 }
