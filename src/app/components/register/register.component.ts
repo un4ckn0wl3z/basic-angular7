81b42ebf5c47aab294c3afca6ad106954525c103
un4ckn0wl3z <slickerth@gmail.com>
2019-08-10 14:24:16 +0700

    complete validate register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup;
+
+  constructor(private builder: FormBuilder) { this.createFormData(); }
 
   ngOnInit() {
+  }
+
+  /** register form data */
+  private createFormData(){
+    this.form = this.builder.group({
+      firstname: ['',[Validators.required]],
+      lastname: ['',[Validators.required]],
+      username: ['',[Validators.required,Validators.pattern(/^[A-z0-9]{3,10}$/)]],
+      password: ['',[Validators.required,Validators.pattern(/^[A-z0-9]{3,10}$/)]],
+      confirm: [false]
+    });
+  }
+
+  /** handle form data */
+  private saveFormData(){
+    this.form.get('firstname').markAsDirty();
+    this.form.get('lastname').markAsDirty();
+    this.form.get('username').markAsDirty();
+    this.form.get('password').markAsDirty();
+    
+    if(this.form.invalid) return;
+
+    console.log(this.form.value);
   }
 
 }
