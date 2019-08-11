417ae9d95793a6fa0942eb933457409fb40e8f97
un4ckn0wl3z <slickerth@gmail.com>
2019-08-11 08:36:42 +0700

    add nodejs server

---
+++ backend/nodejs/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req,res) => {
+    res.send('Hello');
+});
+
+server.listen(3000, () => {
+    console.log('Server runing...');
+});
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -30,7 +30,7 @@ export class HomeComponent implements OnInit  {
   }
 
   ngOnInit(): void {
-    this.service2.getJsonFromPhp().subscribe(res => {
+    this.service2.getAlbum().subscribe(res => {
       this.albums = res;
     });
   }
